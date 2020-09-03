1c3354df89d3878c621fbbcd0f2d46d50ce518fd
Mikael_Nilsson_Enfo <mikael.nilsson@enfo.se>
2020-09-03 13:11:35 +0200

    created class to be read

--- DLLToRead/Class1.cs
+++ DLLToRead/Class1.cs
@@ -4,5 +4,22 @@ namespace DLLToRead
 {
     public class Class1
     {
+        float parm {get; set;}
+        public Class1(float parm1) {
+            parm = parm1;
+        }
+
+        public Class1() {
+            parm = 0;
+        }
+
+        public float sum(float a, float b) {
+            return a + b;
+        }
+
+        public float addToParm(float adder) {
+            parm += adder;
+            return parm;
+        }
     }
 }
