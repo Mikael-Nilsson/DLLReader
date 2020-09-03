aa866abb87482648cbd9e554570db00ae5c193da
Mikael_Nilsson_Enfo <mikael.nilsson@enfo.se>
2020-09-03 13:12:41 +0200

    Created functionality to read DLL

--- DLLReader/Program.cs
+++ DLLReader/Program.cs
@@ -7,7 +7,35 @@ namespace DLLReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("We will read the compiled DLL of the DLLToRead project");
+
+            
+            string path = "<Add full path to DLL here>";
+
+            Assembly asm = Assembly.LoadFile(path);
+            Type[] typeArray = asm.GetTypes();
+
+            Type Class1 = asm.GetType("DLLToRead.Class1");
+
+            // Creating object with no parameters to constructor
+            object classInstance = Activator.CreateInstance(Class1);
+
+            // Calling sum method 
+            MethodInfo sum = Class1.GetMethod("sum");
+            object[] funcParms1 = {2,3};
+            float result = (float)sum.Invoke(classInstance, funcParms1);
+            Console.WriteLine("The result is " + result.ToString());
+
+            // Creating object with parameters to constructor
+            object[] parm2 = {2};
+            object otherInstance = Activator.CreateInstance(Class1, parm2);
+
+            // Calling addToParm method
+            MethodInfo addToParm = Class1.GetMethod("addToParm");
+            object[] funcParms2 = {2};
+            result = (float)addToParm.Invoke(otherInstance, funcParms2);
+
+            Console.WriteLine("The result of addToParm is " + result.ToString());
         }
     }
 }
